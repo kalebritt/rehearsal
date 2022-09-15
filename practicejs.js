d7a4f1ba347257264ee2e93aae1bebd8a8fc88e4
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-15 07:32:42 -0600

    created converter.js and moved module.exports object from practicejs to convertersjs

--- practicejs.js
+++ practicejs.js
@@ -35,12 +35,6 @@ console.log(
   `${celciusInput} degrees Celcius = ${farenheitValue} degrees Farenheit.`
 );
 
-//mode.exports object
-function celciusToFarenheit(celcius) {
-  return celcius * (9 / 5) + 32;
-}
 
-module.exports.celciusToFarenheit = celciusToFarenheit;
-module.exports.farenheitToCelcius = function (farenheit) {
-  return (farenheit - 32) * (5 / 9);
-};
+//require 
+const converters = require('./converters')
