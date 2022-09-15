6c70be309d83f898a270d671cc78313b14f023ec
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-15 16:54:46 -0600

    finished celcius to farenheit code block in same js file

--- celcius-to-farenheit.js
+++ celcius-to-farenheit.js
@@ -1,2 +1,6 @@
 const { celciusToFarenheit } = require('./converters');
 
+const celciusInput = process.argv[2];
+const farenheitValue = celciusToFarenheit(celciusInput);
+
+console.log(`${celciusInput} degrees Celcius = ${farenheitValue} degrees Farenheit.`);
