f9a4640c5bb8852460210486cd6989a622e8cb4d
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-14 19:45:51 -0600

    function practice in js file

--- practicejs.js
+++ practicejs.js
@@ -6,3 +6,16 @@ while (count <= 10) {
 console.log(total);
 
 // console.log(sum(range(1, 10)));
+
+function celciusToFarenheit(celcius) {
+    return celcius * (9/5) + 32;
+}
+
+const freezingPointC = 0;
+const boilingPointC = 100;
+
+const freezingPointF = celciusToFarenheit(freezingPointC);
+const boilingPointF = celciusToFarenheit(boilingPointC);
+
+console.log(`The freezing point water is ${freezingPointF}`);
+console.log(`The boiling point of water is ${boilingPointF}`);
