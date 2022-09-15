c00060e0948e51aa53ed8b5d7b671146bf869c40
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-15 07:16:20 -0600

    module.exports object in practicejs

--- practicejs.js
+++ practicejs.js
@@ -33,3 +33,13 @@ const celciusInput = process.argv[2]
 const farenheitValue = celciusToFarenheit(celciusInput);
 
 console.log(`${celciusInput} degrees Celcius = ${farenheitValue} degrees Farenheit.`);
+
+//mode.exports object
+function celciusToFarenheit(celcius) {
+    return celcius * (9/5) + 32;    
+}
+
+module.exports.celciusToFarenheit = celciusToFarenheit;
+module.exports.farenheitToCelcius = function(farenheit) {
+    return (farenheit - 32) * (5/9);
+};
